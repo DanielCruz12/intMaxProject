191970b8908dbd1ffa7dde75b6520c5b5049ca44
daniel <danieldubonxd@gmail.com>
2022-09-16 01:00:31 -0600

    faqs

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,24 +1,17 @@
 import styles from "../style.js";
-import {faqAction} from '../services/faqAction'
 
 const Faq = () => {
   return (
     <div>
-      <div className="container">
+      <div className="relative w-full overflow-hidden">
         <div className={`${styles.flexCenter}`}>
           <div className="w-full px-4">
             <div className="text-center mx-auto mb-[60px] lg:mb-20 max-w-[620px]">
               <span className="font-semibold text-lg text-white mb-2 block">
                 FAQ
               </span>
-              <h2
-                className={styles.heading2}
-              >
-                Any Questions? Answered
-              </h2>
-              <p
-               className={`${styles.paragraph} mt-2`}
-              >
+              <h2 className={styles.heading2}>Any Questions? Answered</h2>
+              <p className={`${styles.paragraph} mt-2`}>
                 There are many variations of passages of Lorem Ipsum available
                 but the majority have suffered alteration in some form.
               </p>
@@ -26,26 +19,26 @@ const Faq = () => {
           </div>
         </div>
 
-        <div className="flex flex-wrap -mx-4" >
-          <div className="w-full lg:w-1/2 px-4">
+        <div className="flex flex-wrap -mx-4">
+          <div className="w-full lg:w-1/2 px-4 relative overflow-hidden ">
             <div
               className="
                 single-faq
                 w-full
                 bg-primary
-                border border-[#F3F4FE]
+                border border-[#cccede]
                 rounded-lg
-                p-5
+                pb-5
                 sm:p-8
                 mb-8
-                wow
-                fadeInUp
+                transition-all duration-500
               "
-              data-wow-delay=".1s
-              "
-              onClick={faqAction}
             >
-              <button className="faq-btn flex items-center w-full text-left">
+              <input
+                type="checkbox"
+                class="peer w-full h-24 absolute top-0 inset-x-0 z-10 cursor-pointer opacity-0"
+              ></input>
+              <div className="flex items-center w-full h-2 text-left transition-all duration-500">
                 <div
                   className="
                     w-full
@@ -59,6 +52,8 @@ const Faq = () => {
                     text-white
                     bg-opacity-5
                     mr-5
+                    transition-transform duration-500
+                    rotate-0 peer-checked:rotate-180
                   "
                 >
                   <svg
@@ -74,146 +69,24 @@ const Faq = () => {
                     />
                   </svg>
                 </div>
+
                 <div className="w-full">
                   <h4 className="text-base sm:text-lg font-semibold text-white">
-                    How to use TailGrids?
+                    Can I use this template for commercial project?
                   </h4>
                 </div>
-              </button>
-              <div className="faq-content pl-[62px] ">
-                <p className="text-base text-white leading-relaxed py-3">
+              </div>
+              <div className="pl-[62px] transition-all duration-500 hidden peer-checked:block">
+                <p className="text-base text-white leading-relaxed py-3 transition-all duration-500">
                   It takes 2-3 weeks to get your first blog post ready. That
                   includes the in-depth research & creation of your monthly
                   content marketing strategy that we do before writing your
-                  first blog post, Ipsum available .
+                  first blog post, Ipsum available.
                 </p>
               </div>
             </div>
 
-            <div
-              className="
-                single-faq
-                w-full
-                bg-primary
-                border border-[#F3F4FE]
-                rounded-lg
-                p-5
-                sm:p-8
-                mb-8
-                wow
-                fadeInUp
-              "
-              data-wow-delay=".15s
-              "
-              onClick={faqAction}
-            >
-              <button className="faq-btn flex items-center w-full text-left">
-                <div
-                  className="
-                    w-full
-                    max-w-[40px]
-                    h-10
-                    flex
-                    items-center
-                    justify-center
-                    rounded-lg
-                    bg-primary
-                    text-white
-                    bg-opacity-5
-                    mr-5
-                  "
-                >
-                  <svg
-                    width="17"
-                    height="10"
-                    viewBox="0 0 17 10"
-                    className="fill-current icon"
-                  >
-                    <path
-                      d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
-                      fill="#3056D3"
-                      stroke="#3056D3"
-                    />
-                  </svg>
-                </div>
-                <div className="w-full">
-                  <h4 className="text-base sm:text-lg font-semibold text-white">
-                    How to download icons from LineIcons?
-                  </h4>
-                </div>
-              </button>
-              <div className="faq-content pl-[62px] hidden">
-                <p className="text-base text-white leading-relaxed py-3">
-                  It takes 2-3 weeks to get your first blog post ready. That
-                  includes the in-depth research & creation of your monthly
-                  content marketing strategy that we do before writing your
-                  first blog post, Ipsum available .
-                </p>
-              </div>
-            </div>
-
-            <div
-              className="
-                single-faq
-                w-full
-                bg-primary
-                border border-[#F3F4FE]
-                rounded-lg
-                p-5
-                sm:p-8
-                mb-8
-                wow
-                fadeInUp
-              "
-              data-wow-delay=".2s
-              "
-              onClick={faqAction}
-            >
-              <button className="faq-btn flex items-center w-full text-left">
-                <div
-                  className="
-                    w-full
-                    max-w-[40px]
-                    h-10
-                    flex
-                    items-center
-                    justify-center
-                    rounded-lg
-                    bg-primary
-                    text-white
-                    bg-opacity-5
-                    mr-5
-                  "
-                >
-                  <svg
-                    width="17"
-                    height="10"
-                    viewBox="0 0 17 10"
-                    className="fill-current icon"
-                  >
-                    <path
-                      d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
-                      fill="#3056D3"
-                      stroke="#3056D3"
-                    />
-                  </svg>
-                </div>
-                <div className="w-full">
-                  <h4 className="text-base sm:text-lg font-semibold text-white">
-                    Is GrayGrids part of UIdeck?
-                  </h4>
-                </div>
-              </button>
-              <div className="faq-content pl-[62px] hidden">
-                <p className="text-base text-white leading-relaxed py-3">
-                  It takes 2-3 weeks to get your first blog post ready. That
-                  includes the in-depth research & creation of your monthly
-                  content marketing strategy that we do before writing your
-                  first blog post, Ipsum available .
-                </p>
-              </div>
-            </div>
-
+            
           </div>
 
           <div className="w-full lg:w-1/2 px-4">
@@ -227,10 +100,8 @@ const Faq = () => {
                 p-5
                 sm:p-8
                 mb-8
-                wow
+                
                 fadeInUp
-              "
-              data-wow-delay=".1s
               "
             >
               <button className="faq-btn flex items-center w-full text-left">
@@ -277,132 +148,6 @@ const Faq = () => {
                 </p>
               </div>
             </div>
-
-
-            <div
-              className="
-                single-faq
-                w-full
-                bg-primary
-                border border-[#F3F4FE]
-                rounded-lg
-                p-5
-                sm:p-8
-                mb-8
-                wow
-                fadeInUp
-              "
-              data-wow-delay=".15s
-              "
-            >
-              <button className="faq-btn flex items-center w-full text-left">
-                <div
-                  className="
-                    w-full
-                    max-w-[40px]
-                    h-10
-                    flex
-                    items-center
-                    justify-center
-                    rounded-lg
-                    bg-primary
-                    text-white
-                    bg-opacity-5
-                    mr-5
-                  "
-                >
-                  <svg
-                    width="17"
-                    height="10"
-                    viewBox="0 0 17 10"
-                    className="fill-current icon"
-                  >
-                    <path
-                      d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
-                      fill="#3056D3"
-                      stroke="#3056D3"
-                    />
-                  </svg>
-                </div>
-                <div className="w-full">
-                  <h4 className="text-base sm:text-lg font-semibold text-white">
-                    Do you have plan to releasing Play Pro?
-                  </h4>
-                </div>
-              </button>
-              <div className="faq-content pl-[62px] hidden">
-                <p className="text-base text-white leading-relaxed py-3">
-                  It takes 2-3 weeks to get your first blog post ready. That
-                  includes the in-depth research & creation of your monthly
-                  content marketing strategy that we do before writing your
-                  first blog post, Ipsum available .
-                </p>
-              </div>
-            </div>
-
-
-            <div
-              className="
-                single-faq
-                w-full
-                bg-primary
-                border border-[#F3F4FE]
-                rounded-lg
-                p-5
-                sm:p-8
-                mb-8
-                wow
-                fadeInUp
-              "
-              data-wow-delay=".2s
-              "
-              onClick={faqAction}
-            >
-              <button className="faq-btn flex items-center w-full text-left">
-                <div
-                  className="
-                    w-full
-                    max-w-[40px]
-                    h-10
-                    flex
-                    items-center
-                    justify-center
-                    rounded-lg
-                    bg-primary
-                    text-white
-                    bg-opacity-5
-                    mr-5
-                  "
-                >
-                  <svg
-                    width="17"
-                    height="10"
-                    viewBox="0 0 17 10"
-                    className="fill-current icon"
-                  >
-                    <path
-                      d="M7.28687 8.43257L7.28679 8.43265L7.29496 8.43985C7.62576 8.73124 8.02464 8.86001 8.41472 8.86001C8.83092 8.86001 9.22376 8.69083 9.53447 8.41713L9.53454 8.41721L9.54184 8.41052L15.7631 2.70784L15.7691 2.70231L15.7749 2.69659C16.0981 2.38028 16.1985 1.80579 15.7981 1.41393C15.4803 1.1028 14.9167 1.00854 14.5249 1.38489L8.41472 7.00806L2.29995 1.38063L2.29151 1.37286L2.28271 1.36548C1.93092 1.07036 1.38469 1.06804 1.03129 1.41393L1.01755 1.42738L1.00488 1.44184C0.69687 1.79355 0.695778 2.34549 1.0545 2.69659L1.05999 2.70196L1.06565 2.70717L7.28687 8.43257Z"
-                      fill="#3056D3"
-                      stroke="#3056D3"
-                    />
-                  </svg>
-                </div>
-                <div className="w-full">
-                  <h4 className="text-base sm:text-lg font-semibold text-white">
-                    Where and how to host this template?
-                  </h4>
-                </div>
-              </button>
-              <div className="faq-content pl-[62px] hidden">
-                <p className="text-base text-white leading-relaxed py-3">
-                  It takes 2-3 weeks to get your first blog post ready. That
-                  includes the in-depth research & creation of your monthly
-                  content marketing strategy that we do before writing your
-                  first blog post, Ipsum available .
-                </p>
-              </div>
-            </div>
-
           </div>
         </div>
       </div>
